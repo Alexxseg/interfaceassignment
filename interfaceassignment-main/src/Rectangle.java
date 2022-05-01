2d7410ff6f638065826c182cc4c83b83fc622557
Alex Segura <alexsegura72@yahoo.com>
2022-05-01 09:44:05 -0400

    first project image

---
+++ interfaceassignment-main/src/Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle implements Shape{
+
+    public void draw()  {
+
+        System.out.println(" ---");
+        System.out.println("|   |");
+        System.out.println(" ---");
+    }
+    @Override
+    public void Rectangle(){
+
+    }
+
+}
